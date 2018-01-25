b611236a7f98bc27f76ad04acf18ef221218f65f
Harshit Pant <pantharshit00@gmail.com>
2018-01-25 23:30:53 +0530

    (add) mongoose for database

--- src/start.js
+++ src/start.js
@@ -1,4 +1,25 @@
+import d from 'dotenv';
+import mongoose from 'mongoose';
 import app from './app';
+
+d.config({
+  path: `${__dirname}/../.env`
+});
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(process.env.MONGO_URL);
+
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongo :)');
+});
+
+mongoose.connection.on('error', e => {
+  console.log(`Mongo error -> ${e}`);
+});
+
+// Include models here (for mongoose singleton to work)
+import './models/Users';
 
 const PORT = process.env.PORT || 8080;
 
