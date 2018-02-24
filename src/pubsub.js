afb52d877d7c7f31a66c648f6e7cd28b3bfab715
Harshit Pant <pantharshit00@gmail.com>
2018-02-24 20:46:55 +0530

    feat: Add pubsub using redis

--- src/pubsub.js
+++ src/pubsub.js
@@ -1,3 +1,10 @@
-import { PubSub } from 'graphql-subscriptions'
+import { RedisPubSub } from 'graphql-redis-subscriptions'
 
-export default new PubSub()
+export default new RedisPubSub({
+  connection: {
+    host: process.env.REDIS_HOST || '127.0.0.1',
+    port: process.env.REDIS_PORT || '6379',
+    password: process.env.REDIS_PASS || '',
+    retry_strategy: options => Math.max(options.attempt * 100, 3000)
+  }
+})
