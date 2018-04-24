a555ab85d906590c0e5accb0cc9e14d10659321c
Nicky Meuleman <nicky.meuleman+github@gmail.com>
2018-04-24 17:31:44 +0200

    feat: fetching repos by user (#4)
    * feat: start of fetching repos by user
    
    * feat: fetch repos by userId

--- src/resolvers/repos.js
+++ src/resolvers/repos.js
@@ -53,10 +53,18 @@ export default {
 
       return repos
     },
+    getReposByUser: async (_, { id }, context) => {
+      const result = await Repos.find({ owner: { _id: id } })
+      return result
+    },
     getRepo: async (_, { id }, { user }) => {
       const dbData = await Repos.findById(id)
       const [repoOwner, repoName] = dbData.nameWithOwner.split('/')
-      const { data: { data: { repository } } } = await gitql({
+      const {
+        data: {
+          data: { repository }
+        }
+      } = await gitql({
         query: `
         {
           repository(owner: "${repoOwner}", name: "${repoName}") {
@@ -86,7 +94,9 @@ export default {
         starCount: repository.stargazers.totalCount,
         issues: repository.issues.totalCount,
         license: repository.licenseInfo
-          ? repository.licenseInfo.nickname ? repository.licenseInfo.nickname : repository.licenseInfo.name
+          ? repository.licenseInfo.nickname
+            ? repository.licenseInfo.nickname
+            : repository.licenseInfo.name
           : null,
         homepage: repository.homepageUrl,
         prs: repository.pullRequests.totalCount,
@@ -102,7 +112,11 @@ export default {
         throw new Error('No namewithowner provided')
       }
       const [repoOwner, repoName] = nameWithOwner.split('/')
-      const { data: { data: { repository } } } = await gitql({
+      const {
+        data: {
+          data: { repository }
+        }
+      } = await gitql({
         query: `
         {
           repository(name: "${repoName}", owner: "${repoOwner}") {
--- src/schemas/repos.js
+++ src/schemas/repos.js
@@ -2,6 +2,7 @@ export default `
   type Query {
     getRepos(page:Int): [RepoPreview!]
     getRepo(id:ID!): Repo!
+    getReposByUser(id:ID!): [RepoPreview!]
   }
   type Mutation {
     createRepository(name:String!, nameWithOwner: String!, description: String!): Repo!
